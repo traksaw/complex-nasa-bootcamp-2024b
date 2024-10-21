ae0e2b50349c19ec42d8c75fbe457f904624668e
Waskar Paulino <155832641+traksaw@users.noreply.github.com>
2024-10-21 13:43:25 -0400

    v2.0

--- nasaAPI/js/main.js
+++ nasaAPI/js/main.js
@@ -1,3 +1,5 @@
+import { apiKey } from "./apiKeys.js"
+
 const button = document.querySelector('#beginBtn')
 button.addEventListener('click', getFacilities)
 function getFacilities(){
@@ -14,8 +16,8 @@ fetch(urlUno)
             const siteName = element.center
             const lat = element.location.latitude
             const lon = element.location.longitude
-            const apiKey = API_KEY
-            const urlDos = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${lon}`
+            const api = apiKey
+            const urlDos = `http://api.weatherapi.com/v1/current.json?key=${api}&q=${lat},${lon}`
             const li = document.createElement('li')
             parentItem.appendChild(li)
             // li.textContent = `${siteName} - ${cityName} - ${stateName}`
